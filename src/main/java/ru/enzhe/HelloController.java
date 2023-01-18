89f53a0de1f5663c6a7506653c853cf9bfb31b00
EnzheF <enge5533@gmail.com>
2023-01-18 21:07:27 +0300

    Initial commit

---
+++ src/main/java/ru/enzhe/HelloController.java
@@ -0,0 +1,8 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String sayHello() { return "hello"; }
+}
